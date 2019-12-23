36643c4caa0a95a92f389b257698e0600cbcbbf2
fenfenfen999 <fenfenfen999@163.com>
2019-12-23 13:36:40 +0800

    init github.io

---
+++ README.md
@@ -0,0 +1,3 @@
+# ChuBaoDb-github-Web
+
+A responsive website built by Vue.js and Node.js.
---
+++ static/tinymce/plugins/powerpaste/index.js
@@ -0,0 +1,7 @@
+// Exports the "powerpaste" plugin for usage with module loaders
+// Usage:
+//   CommonJS:
+//     require('tinymce/plugins/powerpaste')
+//   ES2015:
+//     import 'tinymce/plugins/powerpaste'
+require('./plugin.js');
